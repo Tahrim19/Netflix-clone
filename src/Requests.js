eb9fbf4b7e999c6d74e67c06cf5d8c2d28f22582
Tahrim19 <tarimbilal4@gmail.com>
2024-08-31 07:53:01 -0700

    made api request using request component

--- src/Requests.js
+++ src/Requests.js
@@ -1,15 +1,16 @@
-const API_KEY = REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchPopular: `/movie/popular?api_key=${API_KEY}&language=en-US`,
-    fetchTVShows:`tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`,
+    fetchTVShows:`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 
-    fetchComedy:`discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchAction:`discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchDocumentaries:`discover/movie?api_key=${API_KEY}&with_genres=99`,
-    fetchHorror:`discover/movie?api_key=${API_KEY}&with_genres=27`,
-    
+    fetchComedy:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchAction:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchDocumentaries:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchHorror:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchMovies:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`,
+    fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
 }
 
 export default requests;
@@ -18,3 +19,4 @@ export default requests;
 
 
 //https://api.themoviedb.org/3/discover/movie?api_key=9c054a8b4bc0794b4716b90bed16c6fa
+// fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
